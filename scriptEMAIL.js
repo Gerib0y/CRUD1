6f8f14450a8c3232f7733b439d970ebed7c4766b
Gabriel Paglia <126928543+Gerib0y@users.noreply.github.com>
2023-06-28 22:17:24 -0300

    Add files via upload

---
+++ scriptEMAIL.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+  document.getElementById("email").style.display = "inline-block"
+  document.getElementById("mail").style.display = "inline-block"
+  document.getElementById("botENVIAR").style.display = "inline-block"
+  document.getElementById("voltar").style.display = "inline-block"
+  }
+)
+
---
+++ scriptINDEX.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+    document.getElementById("usuario").style.display = "inline-block"
+    document.getElementById("nome").style.display = "inline-block"
+    document.getElementById("pass").style.display = "inline-block"
+    document.getElementById("senha").style.display = "inline-block"
+    document.getElementById("botENTRAR").style.display = "inline-block"
+    document.getElementById("esqueci").style.display = "inline-block"
+    }
+  )
+  
